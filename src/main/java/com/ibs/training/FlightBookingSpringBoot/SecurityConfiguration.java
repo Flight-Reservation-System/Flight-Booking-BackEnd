3c7d0ee6356da1551a3cbeb6860cee7d8642ca64
aswinpmadhu <pmadhuaswin666@gmail.com>
2021-07-06 11:57:33 +0530

    First Commit

---
+++ src/main/java/com/ibs/training/FlightBookingSpringBoot/SecurityConfiguration.java
@@ -0,0 +1,27 @@
+package com.ibs.training.FlightBookingSpringBoot;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+@EnableWebSecurity
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+	@Override
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+		// Configuration is set on the auth object
+		auth.inMemoryAuthentication()
+		.withUser("litmus")
+		.password("litmus")
+		.roles("USER")
+		.and()
+		.withUser("trainer")
+		.password("trainer")
+		.roles("ADMIN");
+	}
+@Bean
+public PasswordEncoder getPasswordEncoder() {
+	return NoOpPasswordEncoder.getInstance();
+}
+}
